a398e03493c0eea4dd1e30e85fab045bd106e18d
Dominic Cleal <dominic@cleal.org>
2017-01-16 15:22:44 +0000

    (PDOC-159) Fix lookup of namespaced objects with basic URL decoding
    In server mode, viewing namespaced objects such as foo::bar Puppet
    classes would fail with a 404 due to YARD generating URL-encoded links
    such as `/docs/<module>/puppet_classes/foo_3A_3A_bar`.
    
    During lookup in the YARD registry of the object, no URL decoding is
    performed as usually namespace separators are removed by YARD. In Puppet
    Strings the namespace is retained, so must be decoded.

--- lib/puppet-strings/yard.rb
+++ lib/puppet-strings/yard.rb
@@ -94,3 +94,15 @@ class YARD::CLI::Stats
     [objs.size, undoc.size]
   end
 end
+
+# Monkey patch URL decoding in object displays. Usually :: is interpreted as a
+# namespace, but this is disabled in our base object, and so instead gets
+# URL-encoded.
+require 'yard/server/commands/display_object_command'
+class YARD::Server::Commands::DisplayObjectCommand
+  private
+  alias_method :object_path_yard, :object_path
+  def object_path
+    object_path_yard.gsub('_3A', ':')
+  end
+end
